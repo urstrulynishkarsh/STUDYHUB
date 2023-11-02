c5bb5339b831f66b68f2b81e33d202d59f552771
urstrulynishkarsh <206301010@gkv.ac.in>
2023-11-02 10:47:27 +0530

    done

--- server/index.js
+++ server/index.js
@@ -27,12 +27,12 @@ database.connect();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(
-    cors({
-        origin:'http://localhost:3000',
-        credentials:true,
-    }
-));
+// app.use(
+//     cors({
+//         origin:'http://localhost:3000',
+//         credentials:true,
+//     }
+// ));
 app.use(cors());
 app.use(
 	fileUpload({
